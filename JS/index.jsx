da52bdaf6074d283abbca6e396476bbb1d6d764a
Santiago David <santiagodaviduno@hotmail.com>
2022-02-04 20:45:47 -0400

    Update index.jsx

--- JS/index.jsx
+++ JS/index.jsx
@@ -1,3 +1,1 @@
 ReactDOM.render(<Contador />, document.querySelector("#root"));
-//ReactDOM.render(<Xd />, document.querySelector("#consol"));
-//ReactDOM.render(<Request />, document.querySelector("#request")); */
