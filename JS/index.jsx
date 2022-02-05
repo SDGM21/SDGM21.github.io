4c25ab4897021e34c3cd23145f2aec046e700fa9
Santiago David <santiagodaviduno@hotmail.com>
2022-02-04 20:35:32 -0400

    testings 0.1

---
+++ JS/index.jsx
@@ -0,0 +1,3 @@
+ReactDOM.render(<Contador />, document.querySelector("#root"));
+ReactDOM.render(<Xd />, document.querySelector("#consol"));
+ReactDOM.render(<Request />, document.querySelector("#request"));
